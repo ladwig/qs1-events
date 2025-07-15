9ec3e7d85f60ab329d53d231fcffff43cb498a2a
Daniel Ladwig <ladwig.daniel@icloud.com>
2025-07-15 21:58:28 +0200

    added images

--- src/app/data/artists.js
+++ src/app/data/artists.js
@@ -5,7 +5,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "súz is all about fun, sexy energy on the dancefloor. She doesn't focus on one genre—her sets are driven by a playful, percussive vibe that keeps the crowd moving.\nWhen you ask súz what music she plays, she'll tell you: \"fun and sexy\". From Poland's underground minimal and progressive scene to UK's bass and breaks culture, and now to Berlin's deep, rolling techno, she blends a wide range of influences, always with one goal: to get people dancing.\n\nStarting out as DJ Suzie, she made her mark in Glasgow's scene, exploring a mix of genres. Now based in Berlin, súz plays with tribal percussions, deep textures, and a rolling sound that reflects her mature, refined style.",
-    imageUrl: '',
+    imageUrl: '/artists/suz/suz-1.webp',
     pressPackUrl: '/press/suz-press-kit.zip',
     socialLinks: {
       instagram: 'https://www.instagram.com/suz.dj_',
@@ -18,7 +18,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'MANAGEMENT + BOOKINGS',
     description: "Berlin-based rising talent Freigeist, a dedicated vinyl enthusiast, is a long-standing close associate of QS1. With his recently founded label 'Lengua Materna', he's created a peer-to-peer platform championing local House and Techno artists. His vinyl-only approach and deep appreciation for 90s and 2000s House and Techno has earned him a devoted following in Berlin's clubbing scene. A true selector at heart, Freigeist meticulously curates each set from his vast record collection, ensuring no two performances are alike.\n\nRenowned for his extended closing sets, he's become synonymous with emotional journeys through sound. His performances feature forgotten classics, hidden gems, and timeless anthems - creating an atmosphere where tears of joy and smiles are inevitable.",
-    imageUrl: 'https://static.ra.co/images/profiles/square/freigeist.jpg',
+    imageUrl: '/artists/freigeist/freigeist-1.webp',
     pressPackUrl: '/press/freigeist-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://www.soundcloud.com/freigeist_music',
@@ -31,7 +31,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'MANAGEMENT + BOOKINGS',
     description: "There is no filler story to his background. Hard digging for the best music, endless amount of hours honing and perfecting his technical ability. When on stage, Josh delivers the perfect blend of music to give the dance floor exactly what they need.\nHaving spent the last 10 years building his music library in vinyl and digital formats. Constantly fine tuning his sound and broadening the palette from which he can work from for his DJ sets. Having built himself up primarily in Dublin, working with his own collective and playing regular raves and in iconic clubs such as The Twisted Pepper, The Grand Social and The Button Factory, then travelling further afield with various residencies and performances in different clubs and cities from Berlin to Auckland\nJosh delivers exactly what he promises without fail and constantly shows us that his work and research is always developing and evolving. His style and signature sound is simple, flawlessly executing the art of Dj'ing with with a professionally crafted taste in punchy, fast paced and driving techno",
-    imageUrl: '/artists/josh-reid.jpg',
+    imageUrl: '/artists/josh-reid/josh-reid-1.webp',
     pressPackUrl: '/press/josh-reid-press-kit.zip',
     socialLinks: {
       instagram: 'https://www.instagram.com/joshreid_92',
@@ -44,7 +44,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "Hiss Less. Purr More\nIntroducing a trio of house cats, each bringing an electrifying touch to the decks and redefining the nightlife scene. Meet critical P, Jean-Jez, and Mesmer – a powerhouse team that blends their distinct styles to create an unforgettable experience.\n\ncritical P dominates with a raw, old-school sound that pulsates with relentless energy. Her sets chug and ride, invoking the gritty essence of house's golden era, delivering a nostalgic yet invigorating vibe that keeps the crowd moving.\n\nJean-Jez spices things up with deep and groovy elements, heavily influenced by Afro-Latin grooves, soulful house, and a touch of minimal and tech to keep things hot. His vibrant mixes make every track sizzle with life, creating an undeniable groove that ignites a tribal energy across the dance floor.\n\nMesmer, whose sets are a captivating blend of soulful and tribal sounds, interwoven with irresistible disco elements that transport you to a retro paradise. His ability to seamlessly mix genres keeps the dance floor in a perpetual state of blissful euphoria.\n\nTogether, this dynamic trio delivers house music with an eclectic twist, consistently curating thrilling and diverse selections that transcend the ordinary. Their soundscapes are sexy, groovy, and brimming with attitude, making every performance a celebration of music's power to move and inspire. Whether it's a pulsating nightclub, a vibrant festival, or an intimate gathering, critical P, Jean-Jez, and Mesmer turn every event into a memorable dance experience. Get ready to be captivated by the freshest sounds in house music, brought to you by a trio that knows how to hiss less and purr more.",
-    imageUrl: 'https://static.ra.co/images/profiles/square/kedibounce.jpg',
+    imageUrl: '/artists/kedi-bounce/kedi-bounce-1.webp',
     pressPackUrl: '/press/kedi-bounce-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://soundcloud.com/kedi-bounce',
@@ -57,7 +57,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "Female identifying, Queer, Vinyl DJ from Yorkshire and now based in Berlin. \nCo-founder of Garage Girls.\nWith a varied taste in music, DJ NORTHERN plays a selection of dancefloor bangers, ranging from Garage, House, Techno, Breaks and Drum and Bass. DJ NORTHERN has played for the likes of Rinse FM, Buttons, Refuge Worldwide, and HÖR Berlin.\n\nDJ NORTHERN has a passion for queer parties and community and is a co-founder of FLINTA collective and party series, Garage Girls Berlin.",
-    imageUrl: '/artists/dj-northern.jpg',
+    imageUrl: '/artists/dj-northern/dj-northern-1.webp',
     pressPackUrl: '/press/dj-northern-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://soundcloud.com/djnorthern',
@@ -70,7 +70,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "",
-    imageUrl: '/artists/loopmasters.jpg',
+    imageUrl: '/artists/the-loopmasters/the-loopmasters-1.webp',
     pressPackUrl: '/press/loopmasters-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://soundcloud.com/loopmasterrecords',
@@ -83,7 +83,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "DJ Speed is a selector, co-founder of Loopmaster Records, and one half of 'TheLoopmasters'. Through this Alias, he's released numerous records pushing a looping, stripped back and mysterious sound. Renowned for his prowess behind the decks and a deep, diverse music collection, he brings an unmistakable energy to every set.",
-    imageUrl: '',
+    imageUrl: '/artists/dj-speed/dj-speed-1.webp',
     pressPackUrl: '/press/dj-speed-press-kit.zip',
     socialLinks: {
       instagram: 'ps://www.instagram.com/dj.speed_',
@@ -95,7 +95,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "",
-    imageUrl: '',
+    imageUrl: '/artists/dj-frequency/dj-frequency-1.webp',
     pressPackUrl: '/press/dj-frequency-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://soundcloud.com/djfrequency97',
@@ -108,7 +108,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'MANAGEMENT + BOOKINGS',
     description: "Npoint_O is based in Berlin. He is one of the founders of QS1 Berlin collective with which he's been pushing the underground Berlin scene forward both with the creation of events and nurturing of creative spaces.\n\nThe main motivation for his sets is constant journey evolution.",
-    imageUrl: '/artists/npoint_o.jpg',
+    imageUrl: '/artists/npoint-o/npoint-o-1.webp',
     pressPackUrl: '/press/npoint_o-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://soundcloud.com/npoint-o',
@@ -121,7 +121,7 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "Sparkling Water Dreams is an irish DJ and producer emerging in the house music scene with a sound that's both deep and groovy. Based in Berlin, Christopher has built a reputation for crafting punchy, groove-driven house sets that flow effortlessly between progressive, acid and classic house, always maintaining an infectious energy on the dance floor.\nDrawing from influences in classic Chicago house, tech-house, and acid house, Sparkling Water Dreams is known for sets that strike a perfect balance between euphoric atmospheres and bass-heavy rhythms. Their ability to weave through genres keeps crowds moving and fully immersed in the music.\n\nThe DJ has played at iconic venues around Berlin such as Tresor, Watergate,, Sisyphos, Later Blau, Else. Renate, Aeden. About:Blank, Golden Gate, MO1 & more bringing their signature sound to audiences\n\nSparkling Water Dreams' debut EP, Last Call, released on Work Them Records, is a testament to their keen production skills and understanding of house music's roots while pushing the genre forward. With support from well-known DJs like Amotik, Midland, Laurent Garnier. Satoshi Tomiie. DJ Deep & more, the EP has garnered attention for its crisp production, intricate acid, and captivating chords.\n\nAs a DJ, Sparkling Water Dreams is all about reading the room, ensuring each set evolves organically, transitioning from deep house grooves to peak-time House with ease. Whether in the studio or behind the decks, Sparkling Water Dreams is constantly experimenting, reshaping traditional house sounds with a modern twist, making them one of the most exciting new talents in the genre.",
-    imageUrl: '/artists/sparkling-water-dreams.jpg',
+    imageUrl: '/artists/sparkling-water-dreams/sparkling-water-dreams-1.webp',
     pressPackUrl: '/press/sparkling-water-dreams-press-kit.zip',
     socialLinks: {
       soundcloud: 'https://soundcloud.com/sparklingwaterdreams',
@@ -134,10 +134,23 @@ export const artists = [
     basedIn: 'BERLIN, DE',
     type: 'BOOKINGS',
     description: "The Dublin-born DJ who now calls Berlin his musical playground. With an eclectic taste for pumping sounds, T-Rail has gained a reputation as an artist who refuses to be confined by a single genre, a craft he has honed at his QS1 residency & his Berlin based party: Hip Knocker. From hypnotic loops to tripped-out house cuts and everything in between; it's a whirlwind ride from start to finish",
-    imageUrl: '/artists/t-rail.jpg',
+    imageUrl: '/artists/t-rail/t-rail-1.webp',
     pressPackUrl: '/press/t-rail-press-kit.zip',
     socialLinks: {
       instagram: 'https://www.instagram.com/teeerail/'
+    },
+  },
+  {
+    name: 'Beryll',
+    setType: 'DJ SET',
+    basedIn: 'BERLIN, DE',
+    type: 'BOOKINGS',
+    description: "",
+    imageUrl: '/artists/beryll/beryll-1.webp',
+    pressPackUrl: '/press/beryll-press-kit.zip',
+    socialLinks: {
+      instagram: '',
+      soundcloud: ''
     },
   },
 ]; 
