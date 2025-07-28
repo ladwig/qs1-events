850ec4bcce13401811f0ff22264f44359329367d
Daniel Ladwig <ladwig.daniel@icloud.com>
2025-07-28 10:32:45 +0200

    fix soundcloud list

--- src/app/components/SoundCloudModal.js
+++ src/app/components/SoundCloudModal.js
@@ -32,7 +32,7 @@ export default function SoundCloudModal({ onClose }) {
               scrolling="no"
               frameBorder="no"
               allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/1169606926&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/qs1studio&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             />
           </div>
         </div>
