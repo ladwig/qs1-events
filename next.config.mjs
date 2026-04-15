5772baf28489bc51c7ea74e755af104c327798d0
ladwig <ladwig.daniel@icloud.com>
2026-04-15 21:57:20 +0200

    redirect warteliste

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/studio-rental',
+        destination: 'https://docs.google.com/forms/d/e/1FAIpQLSdFVekBip6p7HdiK35M40K5HvmVRpPcg16VYJgitrgMOXGzng/viewform',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
